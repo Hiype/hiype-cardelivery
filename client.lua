f98e632d4e0479e34feea8deb43d925a11a946c4
Hiype <47718546+Hiype@users.noreply.github.com>
2021-10-09 20:49:01 +0300

    Added a config file for easier changes

--- client.lua
+++ client.lua
@@ -1,25 +1,3 @@
-local vehicles = {
-    {model="rs7", name="Audi RS7"},
-    {model="f82", name="BMW M5"},
-    {model="m6f13", name="BMW M6 F13"},
-    {model="bmwe39", name="BMW E39"},
-    {model="subwrx", name="Subaru Impreza WRX STI 2004"},
-}
-
-local spawns = {
-    -- {name="Rancho", heading=302.58, x=548.79, y=-1930.65, z=24.25},
-    -- {name="Cypress flats", heading=179.45, x=998.85, y=-1944.38, z=30.47},
-    -- {name="La mesa inside a garage", heading=263.13, x=946.4, y=-1697.76, z=29.52}
-    {name="", heading=88.11, x=-69.64, y=-1418.39, z=29.33}
-}
-
-local destinations = {
-    {name="Pacific bluffs garage", x=-2011.74, y=-351.87, z=47.69, from=1200, to=1400},
-    {name="Chumash house parking lot", x=-3201.59, y=1152.77, z=9.24, from=1500, to=1700},
-    {name="Vineyard parking lot", x=-1921.22, y=2044.75, z=140.32, from=1800, to=2000},
-    {name="Sandyshores trailer park", x=1828.54, y=3863.04, z=33.28, from=2000, to=2200}
-}
-
 local npcHeading = 205.9
 local npcCoords = vector3(-79.25, -1392.6, 29.32)
 
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,8 +6,9 @@ version '1.0.0'
 
 client_script 'client.lua'
 server_script 'server.lua'
-shared_script 'shared.lua'
 
-shared_script { 
-    '@qb-core/import.lua'
+shared_scripts { 
+    '@qb-core/import.lua',
+    'shared.lua',
+    'config.lua'
 }
